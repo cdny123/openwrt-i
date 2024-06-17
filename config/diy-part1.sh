262a95857f379220a1f5e3adbda1248ba30a1ce4
cdny123 <133751542+cdny123@users.noreply.github.com>
2024-06-17 16:47:05 +0800

    Create diy-part1.sh

---
+++ config/diy-part1.sh
@@ -0,0 +1,5 @@
+# 添加 luci-app-chatgpt 软件包
+git clone https://github.com/jerrykuku/luci-app-chatgpt.git package/luci-app-chatgpt
+
+# 添加 luci-theme-design 主题
+git clone https://github.com/jerrykuku/luci-theme-design.git package/luci-theme-design
