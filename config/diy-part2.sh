1a50fecc64fb0ab818e54b2f5ebaa6ea385f5aa3
cdny123 <133751542+cdny123@users.noreply.github.com>
2024-06-18 08:53:31 +0800

    Update diy-part2.sh

--- config/diy-part2.sh
+++ config/diy-part2.sh
@@ -1,8 +1,12 @@
-# 添加自动更新固件的脚本
-cat <<EOF >> openwrt/package/base-files/files/bin/openwrt_autoupdate.sh
+# 自动更新固件
+cat >> package/base-files/files/bin/firmware_update <<EOF
 #!/bin/sh
-...
+echo "Starting firmware update..."
+wget -qO- "$FIRMWARE_UPDATE_URL" | tar -xzC /
+echo "Firmware update complete. Rebooting device..."
+reboot
 EOF
+chmod +x package/base-files/files/bin/firmware_update
 
 # 添加显示 CPU 内存信息的脚本
 cat <<EOF >> openwrt/package/base-files/files/bin/openwrt_sysinfo.sh
